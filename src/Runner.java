ef6c64341d90067a4a2174efb397b9a15f6064ed
enrique <enriquespicersaave@students.berkeley.net>
2019-02-06 14:02:02 -0800

    Enrique Done With Basic Excercises

---
+++ src/Runner.java
@@ -0,0 +1,17 @@
+public class Runner {
+    public static void main(String[] args) {
+        Car myCar = new Car(2008, "Tesla", 2000, "X" );
+        myCar.setMiles(2020);
+        myCar.toString();
+
+
+
+        Rectangle mySquare = new Rectangle(20, 20);
+        mySquare.getArea();
+        mySquare.getDiagonal();
+        mySquare.isSquare();
+    }
+}
+
+
+
